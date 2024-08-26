e30dc13a32199cd7983d3c14133a07d18781e11c
elvis liao <1219585136@qq.com>
2024-08-26 15:58:09 +0800

    feat: add Include List render controller

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,22 @@
     <component :is="views[activeViewName]" :key="activeViewName" />
   </keep-alive>
 
-  <div>Include List: [ {{ cacheList.join(',') }} ]</div>
+  <div v-if="showCacheList">Include List: [ {{ cacheList.join(',') }} ]</div>
+  <br>
+  <div>Active View: {{ activeViewName }}</div>
   <br>
 
-  <button @click="() => activeViewName = 'Home'" class="link">Index</button>
-  <button @click="() => activeViewName = 'About'" class="link">About</button>
-  <button @click="handleCleanCacheList">Clean keep alive include List</button>
+  <div>
+    <input id="showCacheList" type="checkbox" v-model="showCacheList" />
+    <label for="showCacheList">showCacheList</label>
+  </div>
+  <br>
+
+  <div style="display: flex; gap: 10px">
+    <button @click="() => activeViewName = 'Home'" class="link">Home</button>
+    <button @click="() => activeViewName = 'About'" class="link">About</button>
+    <button @click="handleCleanCacheList">Clean keep alive include List</button>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -18,6 +28,7 @@ import Home from './views/Home.vue';
 import About from './views/About.vue';
 
 const activeViewName = ref('Home')
+const showCacheList = ref(false)
 
 const views: Record<string, any> = {
   Home,
